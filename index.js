beb52ea480c799401fc5be39cf4cde379fdf1f89
Douglas Vaz <douglas1992@gmail.com>
2015-01-27 12:38:17 +0530

    Change passing errors to callback as per node convention

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-var request = require('request')
-, config = {};
+var request = require('request'),
+	config = {};
 
 exports.sandbox = 'http://sandbox.1self.co';
 exports.production = 'https://api.1self.co';
 
-function Stream(streamId, writeToken, readToken){
-	var s = streamId
-	, w = writeToken
-	, r = readToken;
+function Stream(streamId, writeToken, readToken) {
+	var s = streamId,
+		w = writeToken,
+		r = readToken;
 
-	this.send = function(event, sendCallback, sendError) {
-		if (event.objectTags === undefined){
+	this.send = function(event, callback) {
+		if (event.objectTags === undefined) {
 			throw 'object tags must be specified';
-		} else if (event.actionTags === undefined){
+		} else if (event.actionTags === undefined) {
 			throw 'action tags must be specified';
-		} else if (event.properties === undefined){
+		} else if (event.properties === undefined) {
 			throw 'properties must be specified';
 		}
 
 		request({
-            method: 'POST'
-            , uri: config.server + '/v1/streams/' + s
-            , gzip: true
-            , headers: {
-                'Authorization': w
-                , 'Content-type': 'application/json'
-            }
-            , body: event
-        }, function(e, response, body) {
-            if (e){
-            	sendError(e);
-            }
+			method: 'POST',
+			uri: config.server + '/v1/streams/' + s,
+			gzip: true,
+			headers: {
+				'Authorization': w,
+				'Content-type': 'application/json'
+			},
+			body: event
+		}, function(e, response, body) {
+			if (e) {
+				callback(e);
+			}
 
-	        console.log('Logged the event');
-            sendCallback(JSON.parse(body));
-        })
+			console.log('Logged the event');
+			callback(null, JSON.parse(body));
+		})
 	}
 
 	this.toJSON = function() {
 		var result = {
-			streamId: s
-			, writeToken: w
-			, readToken: r
+			streamId: s,
+			writeToken: w,
+			readToken: r
 		};
 
 		return JSON.stringify(result);
 	}
 }
 
-function Lib1selfserver(c){
+function Lib1selfserver(c) {
 	config = {
 		server: 'http://sandbox.1self.co'
 	};
 
-	if (c === undefined ){
+	if (c === undefined) {
 		throw 'config object must be defined';
 	}
-	if (c.appId === undefined){
+	if (c.appId === undefined) {
 		throw 'appId must be specified';
-	} else if (c.appSecret === undefined){
+	} else if (c.appSecret === undefined) {
 		throw 'appSecret must be specified';
 	}
 
@@ -66,25 +66,23 @@ function Lib1selfserver(c){
 	config.appSecret = c.appSecret;
 	config.server = c.server === undefined ? config.server : c.server;
 
-	this.createStream = function(callback, error) {
+	this.createStream = function(callback) {
 		request({
-            method: 'POST'
-            , uri: config.server + '/v1/streams'
-            , gzip: true
-            , headers: {
-                'Authorization': config.appId + ':' + config.appSecret
-            }
-        }, function(e, response, body) {
-            if (e) {
-            	error();
-            }
+			method: 'POST',
+			uri: config.server + '/v1/streams',
+			gzip: true,
+			headers: {
+				'Authorization': config.appId + ':' + config.appSecret
+			}
+		}, function(e, response, body) {
+			if (e) {
+				callback(e);
+			}
 
-            console.log(body);
-            var stream = new Stream(body.streamId
-            						, body.writeToken
-            						, body.readToken);
-            callback(stream);
-        })
+			console.log(body);
+			var stream = new Stream(body.streamId, body.writeToken, body.readToken);
+			callback(null, stream);
+		})
 	}
 }
 
